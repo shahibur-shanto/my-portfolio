3ca1b5ca1152a22f5c87a4a582fd11096c219e3e
shahibur.shanto <shahibur407@gmail.com>
2021-05-03 23:11:22 +0600

    home again

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,9 +7,9 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="home w-100">
-      <Row className="row justify-content-md-center align-items-md-center">
-        <Col className="col-sm-12 col-md-5 mt-auto">
+    <div className="home">
+      <div className="row">
+        <div className="col-sm-12 col-md-5 mt-auto">
           <div className="mt-5">
             <h1 className="me">Hey, I'm Shahibur</h1>
             <Typing>
@@ -20,12 +20,12 @@ const Home = () => {
             </Typing>
             <Link className="btn btn-primary" target="_blank"><a className="resume" href="https://drive.google.com/file/d/1v6cVR0fcSLVb0-h9TUegbu6Zgyq3DQ7s/view?usp=sharing">Download Resume</a></Link>
           </div>
-        </Col>
+        </div>
 
-        <Col className="col-sm-12 col-md-5 mt-auto">
+        <div className="col-sm-12 col-md-5 mt-auto">
          <div className="text-center"> <img className="my-image img-fluid" src={me} alt="" /></div>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   );
 };
